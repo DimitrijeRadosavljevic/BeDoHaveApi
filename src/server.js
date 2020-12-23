6284e597057effc8b268ed383911a7450a0aea7f
Cvetanovic Goran <cvetanovicgoran99@gmail.com>
2020-12-24 00:02:47 +0100

    Project conected with neo4j database

--- src/server.js
+++ src/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import {json, urlencoded} from 'body-parser'
 import morgan from 'morgan'
+import * as userRepository from "./Repositories/userRepository"
 const port = 3000;
 
 export const app = express()
@@ -14,6 +15,22 @@ app.use(json())
 app.use(urlencoded({extended: true}))
 // Logging middleware, example POST /next 200 5.868 ms - 19
 app.use(morgan('dev'))
+
+app.get('/api/getSlovca', (req, res) => {
+  res.status(200).send({ slovca: "Slovca" });
+})
+
+app.post('/api/postSlovca', (req, res) => {
+  res.status(300).send( { data: req.body})
+})
+
+app.post('/api/postPerson', async function (req, res) {
+
+  const data =  await userRepository.createPerson354(req.body.name);
+  res.status(200).send( { data: "Person iz created" , person: data });
+  //return 700000;
+
+})
 
 
 export const start = () => {
